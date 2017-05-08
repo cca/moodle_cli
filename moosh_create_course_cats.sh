5d196b09d201ac8062551612c6965a0539ace06e
phette23 <phette23@gmail.com>
2017-05-08 12:55:44 -0700

    moosh script to create term's course categories

---
+++ moosh_create_course_cats.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create course categories for a given term
+# Run this once per term at the beginning
+
+echo -n "Enter semester (in form '2017SU'):"
+read term
+
+# meow
+cats=(Metacourses ANIMA ARCHT ARTED CERAM COMAR COMIC CORES CRAFT CRITI CRTSD CURPR DESGN DIVSM DIVST DSMBA FASHN FILMG FILMS FINAR FNART FURNT GELCT GLASS GRAPH ILLUS INDIV INDUS INTER IXDGR IXDSN LITPA MARCH METAL PHCRT PHOTO PNTDR PRINT SCIMA SCULP SFMBA SSHIS TEXTL UDIST VISCR VISST WRITE WRLIT)
+
+# moosh category-create returns the ID of the category
+echo "Creating course category for ${term}..."
+parent=$(moosh -n category-create -v 1 ${term})
+
+for cat in ${cats[*]}; do
+    echo "Creating course category for ${cat}..."
+    moosh -n category-create -p ${parent} -v 1 $cat
+done
