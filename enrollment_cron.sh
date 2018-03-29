9e0a4f7b424b8dd0c30df2938d8b6e934f0e1a4e
phette23 <phette23@gmail.com>
2018-03-29 13:32:24 -0700

    update enrollment cron script

--- enrollment_cron.sh
+++ enrollment_cron.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+echo "$(date) - running Moodle enrollment script"
 # set "unenroll action" to "unenroll user from course"
 moosh -n config-set unenrolaction 0 enrol_database
 # sync users first and then enrollments
-/usr/bin/php auth/db/cli/sync_users.php
-/usr/bin/php enrol/database/cli/sync.php
+sudo -u www-data /usr/bin/php /opt/moodle/auth/db/cli/sync_users.php -v
+sudo -u www-data /usr/bin/php /opt/moodle/enrol/database/cli/sync.php -v
 # set unenroll action _back_ to "keep user enrolled"
 moosh -n config-set unenrolaction 1 enrol_database
+echo "$(date) - enrollment script finished"
