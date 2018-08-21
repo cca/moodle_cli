0313af345d6cd6ab28c99913c40150daf9beea90
phette23 <phette23@gmail.com>
2018-08-21 14:43:55 -0700

    fix enroll script
    every executable should be a full path
    every line run as www-data user

--- enrollment_cron.sh
+++ enrollment_cron.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 echo "$(date) - running Moodle enrollment script"
+cd /opt/moodle
 # set "unenroll action" to "unenroll user from course"
-moosh -n config-set unenrolaction 0 enrol_database
+sudo -u www-data /usr/local/bin/moosh config-set unenrolaction 0 enrol_database
 # sync users first and then enrollments
 sudo -u www-data /usr/bin/php /opt/moodle/auth/db/cli/sync_users.php -v
 sudo -u www-data /usr/bin/php /opt/moodle/enrol/database/cli/sync.php -v
 # set unenroll action _back_ to "keep user enrolled"
-moosh -n config-set unenrolaction 1 enrol_database
+sudo -u www-data /usr/local/bin/moosh config-set unenrolaction 1 enrol_database
 echo "$(date) - enrollment script finished"
