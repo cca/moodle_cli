667e3e44daabd1544277cc418c9061d0fa9f57b3
phette23 <phette23@gmail.com>
2019-08-26 14:40:20 -0700

    update enrollment script, fix external db problem
    auth/db/cli/sync_users.php was doing nothing because the "db" auth was
    not enabled, but we also don't want it permanently enabled. So now we
    enable it, sync users, disable it, change the auth type from db to cas
    of the newly created users.

--- enrollment_cron.sh
+++ enrollment_cron.sh
@@ -4,8 +4,15 @@ echo "$(date) - running Moodle enrollment script"
 cd /opt/moodle
 # set "unenroll action" to "unenroll user from course"
 sudo -u www-data /usr/local/bin/moosh config-set unenrolaction 0 enrol_database
-# sync users first and then enrollments
+# we sync users first and then enrollments
+# we enable auth db plugin to sync users, then disable it because we won't want
+# people actually signing in via external database
+# we'll change all their accounts to CAS later
+sudo -u www-data /usr/local/bin/moosh auth-manage enable db
 sudo -u www-data /usr/bin/php /opt/moodle/auth/db/cli/sync_users.php -v
+sudo -u www-data /usr/local/bin/moosh auth-manage disable db
+sudo -u www-data /usr/bin/php /opt/moodle/admin/cca_cli/cca_set_cas_logins.php
+
 sudo -u www-data /usr/bin/php /opt/moodle/enrol/database/cli/sync.php -v
 # set unenroll action _back_ to "keep user enrolled"
 sudo -u www-data /usr/local/bin/moosh config-set unenrolaction 1 enrol_database
