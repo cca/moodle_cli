ce6f16484e4801f9e1222c8b2d8ef20de4122aca
phette23 <phette23@gmail.com>
2019-09-09 12:43:03 -0700

    enroll cron - silence sync_users errors

--- enrollment_cron.sh
+++ enrollment_cron.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# silence auth db plugin sync message or enrol.log fills up with them
+MSG="user with this username was already created through 'cas' plugin."
 echo "$(date) - running Moodle enrollment script"
 cd /opt/moodle
 # set "unenroll action" to "unenroll user from course"
@@ -9,7 +11,7 @@ sudo -u www-data /usr/local/bin/moosh config-set unenrolaction 0 enrol_database
 # people actually signing in via external database
 # we'll change all their accounts to CAS later
 sudo -u www-data /usr/local/bin/moosh auth-manage enable db
-sudo -u www-data /usr/bin/php /opt/moodle/auth/db/cli/sync_users.php -v
+sudo -u www-data /usr/bin/php /opt/moodle/auth/db/cli/sync_users.php -v | sed "/$MSG/d"
 sudo -u www-data /usr/local/bin/moosh auth-manage disable db
 sudo -u www-data /usr/bin/php /opt/moodle/admin/cca_cli/cca_set_cas_logins.php
 
