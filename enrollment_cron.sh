10392c0435436f7f5043cbe2708cd322b01ab901
phette23 <phette23@gmail.com>
2019-10-09 12:49:04 -0700

    enrollment - email admins if anything fails
    also a bit of code cleanup

--- enrollment_cron.sh
+++ enrollment_cron.sh
@@ -1,21 +1,36 @@
 #!/usr/bin/env bash
 
-# silence auth db plugin sync message or enrol.log fills up with them
+# pipe fails if any command in it fails (needed for sync_users.php | sed cmd)
+set -o pipefail
+ADMINS="ephetteplace@cca.edu,bobbywhite@cca.edu,nlammiller@cca.edu"
+# we will silence this auth db plugin message (else enroll.log fills up)
 MSG="user with this username was already created through 'cas' plugin."
+moosh () { sudo -u www-data /usr/local/bin/moosh $@; }
+php () { sudo -u www-data /usr/bin/php $@; }
+
 echo "$(date) - running Moodle enrollment script"
 cd /opt/moodle
 # set "unenroll action" to "unenroll user from course"
-sudo -u www-data /usr/local/bin/moosh config-set unenrolaction 0 enrol_database
+moosh config-set unenrolaction 0 enrol_database
 # we sync users first and then enrollments
 # we enable auth db plugin to sync users, then disable it because we won't want
 # people actually signing in via external database
 # we'll change all their accounts to CAS later
-sudo -u www-data /usr/local/bin/moosh auth-manage enable db
-sudo -u www-data /usr/bin/php /opt/moodle/auth/db/cli/sync_users.php -v | sed "/$MSG/d"
-sudo -u www-data /usr/local/bin/moosh auth-manage disable db
-sudo -u www-data /usr/bin/php /opt/moodle/admin/cca_cli/cca_set_cas_logins.php
+moosh auth-manage enable db
+php auth/db/cli/sync_users.php -v | sed "/$MSG/d"
+SYNC_STATUS=$?
+moosh auth-manage disable db
+php admin/cca_cli/cca_set_cas_logins.php
 
-sudo -u www-data /usr/bin/php /opt/moodle/enrol/database/cli/sync.php -v
+php enrol/database/cli/sync.php -v
+ENROL_STATUS=$?
 # set unenroll action _back_ to "keep user enrolled"
-sudo -u www-data /usr/local/bin/moosh config-set unenrolaction 1 enrol_database
+moosh config-set unenrolaction 1 enrol_database
 echo "$(date) - enrollment script finished"
+
+# if either command failed, email administrators
+if [ $SYNC_STATUS -ne 0 -o $ENROL_STATUS -ne 0 ]; then
+    echo "Script failure. auth/db/cli/sync_users.php status: ${SYNC_STATUS} | enrol/database/cli/sync.php status: ${ENROL_STATUS}"
+    echo "Emailing ${ADMINS}"
+    mail -s 'Moodle: Enrollment Script Error' -a /var/log/moodle/enroll.log $ADMINS < echo 'See attached log file.'
+fi
