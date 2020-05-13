a7ef4f372aa1fcc2b49df1c696db1e65e7714632
phette23 <phette23@gmail.com>
2020-05-13 15:19:17 -0700

    enrollment - CLI scripts are deprecated, run as scheduled tasks

--- enrollment_cron.sh
+++ enrollment_cron.sh
@@ -17,14 +17,14 @@ moosh config-set unenrolaction 0 enrol_database
 # people actually signing in via external database
 # we'll change all their accounts to CAS later
 moosh auth-manage enable db
-php auth/db/cli/sync_users.php -v | sed "/$MSG/d"
+php admin/tool/task/cli/schedule_task.php --execute=\auth_db\task\sync_users
 # unfortunately sync_users.php returns 0 if it cannot access its db but this will
 # catch some other errors possibly
 SYNC_STATUS=$?
 moosh auth-manage disable db
 php admin/cca_cli/cca_set_cas_logins.php
 
-php enrol/database/cli/sync.php -v
+php admin/tool/task/cli/schedule_task.php --execute=\enrol_database\task\sync_enrolments
 ENROL_STATUS=$?
 echo 'Setting "unenroll action" back to "keep user enrolled"'
 moosh config-set unenrolaction 1 enrol_database
