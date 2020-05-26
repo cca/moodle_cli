4f4b55e857c98ddbff5f35741ed3628e507853b3
phette23 <phette23@gmail.com>
2020-05-26 08:40:50 -0700

    courses in semester sql - use curly braces instead of table prefix

--- courses_in_semester.sql
+++ courses_in_semester.sql
@@ -1,7 +1,7 @@
 SELECT parents.name as parent_category_name, cc.name as category_name, COUNT(*) as course_count
-FROM mdl_course c
-JOIN mdl_course_categories cc ON (c.category = cc.id)
-LEFT JOIN mdl_course_categories parents ON (cc.parent = parents.id)
+FROM {course} c
+JOIN {course_categories} cc ON (c.category = cc.id)
+LEFT JOIN {course_categories} parents ON (cc.parent = parents.id)
 WHERE c.visible = 1
 AND parents.name != "Communities"
 GROUP BY parents.name, cc.name
