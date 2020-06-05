9386e2732baf89bcb2677ce5d3f9d1470f032c8e
phette23 <phette23@gmail.com>
2020-06-05 12:38:54 -0700

    sandbox script, fix parameter quoting

--- sandbox.sh
+++ sandbox.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
 # create a sandbox courses for a department
-moosh () { sudo /usr/local/bin/moosh -n $@; }
 
 if [ $1 = '-h' -o $1 = 'help' -o $1 = '--help' ]; then
     echo "usage: sandbox.sh DEPARTMENT_CODE DEPARTMENT_NAME"
@@ -14,4 +12,4 @@ DEPARTMENT_CODE="$1"
 COURSE_SHORTNAME="${DEPARTMENT_CODE}-SANDBOX"
 DEPARTMENT_NAME="$2"
 
-moosh course-create --category ${SANDBOXES_CATEGORY_ID} --fullname "${DEPARTMENT_NAME} Sandbox" --idnumber ${COURSE_SHORTNAME} ${COURSE_SHORTNAME}
+moosh -n course-create --category=${SANDBOXES_CATEGORY_ID} --fullname="${DEPARTMENT_NAME} Sandbox" --idnumber="${COURSE_SHORTNAME}" "${COURSE_SHORTNAME}"
