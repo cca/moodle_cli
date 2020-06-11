f64a2a34ac32dc7555ada19e8f0ba97fcd516bb2
phette23 <phette23@gmail.com>
2020-06-11 11:09:54 -0700

    fix sendmail script, accept stdin msg

--- sendemail.php
+++ sendemail.php
@@ -3,20 +3,21 @@
 // https://docs.moodle.org/dev/CLI_scripts
 define('CLI_SCRIPT', true);
 
-require(__DIR__.'/../../../config.php');
+require(__DIR__.'/../../config.php');
 require_once($CFG->libdir.'/clilib.php'); // cli only functions
 require_once($CFG->libdir.'/moodlelib.php'); // has email_to_user()
-require_once(__DIR__.'/../../../user/lib.php'); // has user_get_users_by_id()
+require_once(__DIR__.'/../../user/lib.php'); // has user_get_users_by_id()
 
-// Systems Librarian userid = 4, fn accepts & returns arrays
-$syslibUser = user_get_users_by_id(array(4))[0];
+// user_get_users_by_id() accepts an array of IDs and returns an _associative
+// array indexed by ID_ so do not try to access its single entry with "[0]"
+$syslibID = 4;
+$syslibUser = user_get_users_by_id(array($syslibID))[$syslibID];
 // https://docs.moodle.org/dev/Data_manipulation_API#General_concepts
 // another way to get user is $DB->get_record('user',['email' => '...'])
-// but does this return a user _object_ or mere assoc array?
 $subject = "Test Moodle Email Subject";
-$msg = "Plain text message. We will turn this into a CLI parameter.";
+$msg = stream_get_contents(STDIN);
 
-$status = email_to_user($syslibUser, $syslibUser, $subject, $msg, false);
+$status = email_to_user($syslibUser, $syslibUser, $subject, $msg);
 if ($status) {
     echo("Email sent successfully.");
     exit;
