b07f6d69699f6e012cbd435c4b1731bb194f9b6a
phette23 <phette23@gmail.com>
2020-07-20 13:47:05 -0700

    enrol - add missing faculty to their cohort
    routine runs after user sync, test by manually running these commands
    but we'll see if works tonight

--- enrollment_cron.sh
+++ enrollment_cron.sh
@@ -28,6 +28,32 @@ php admin/tool/task/cli/schedule_task.php --execute='\enrol_database\task\sync_e
 ENROL_STATUS=$?
 echo 'Setting "unenroll action" back to "keep user enrolled"'
 moosh config-set unenrolaction 1 enrol_database
+
+# add users who are instructors of a course but not already in the FACULTY
+# (id = 2) cohort, nor in the Faculty EXCEPTION (id = 3) cohort, to the Faculty
+# cohort. Run after user and enrollment sync.
+echo 'Checking users for instructors who are not in the FACULTY cohort...'
+
+# see the "List of Instructors not in Faculty Cohort" ad-hoc database query
+# for this SQL - https://moodle.cca.edu/report/customsql/view.php?id=8
+# `moosh sql-run` returns text formatted like this:
+# Record 8
+# stdClass Object
+# (
+#     [id] => 25152
+# )
+# so we use `sed` to get only the ID number itself
+USERS=$(moosh -n sql-run 'SELECT user.id FROM {user} user JOIN {role_assignments} ra ON user.id = ra.userid JOIN (SELECT * FROM {context} WHERE contextlevel = 50) context ON ra.contextid = context.id JOIN {role} role ON ra.roleid = role.id LEFT JOIN (SELECT * FROM {cohort_members} cms WHERE cms.cohortid = 2 OR cms.cohortid = 3) cm ON user.id = cm.userid WHERE role.id = 3 AND cm.id IS NULL GROUP BY username ORDER BY username ASC' | sed -n "/\[id\] =>/s/ \+\[id\] => //p")
+
+if test -n "${USERS}"; then
+    echo "Found $(echo "${USERS}" | wc -l) missing faculty."
+    for USER in ${USERS}; do
+        moosh -n cohort-enrol -u ${USER} "FACULTY"
+    done
+else
+    echo 'No users missing from Faculty cohort.'
+fi
+
 echo "$(date) - enrollment script finished"
 
 # if either command failed, email administrators
