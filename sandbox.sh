96c134cfa39cb8efdfbaa4c7b90f06589eb6f8dd
phette23 <phette23@gmail.com>
2020-07-29 10:53:22 -0700

    sandbox - add an instructor if username is given

--- sandbox.sh
+++ sandbox.sh
@@ -2,8 +2,9 @@
 # create a sandbox courses for a department
 
 if [ $1 = '-h' -o $1 = 'help' -o $1 = '--help' ]; then
-    echo "usage: sandbox.sh DEPARTMENT_CODE DEPARTMENT_NAME"
-    echo "example: sandbox.sh ANIMA Animation"
+    echo "usage: sandbox.sh DEPARTMENT_CODE DEPARTMENT_NAME [FACULTY_USERNAME]"
+    echo "example: sandbox.sh ANIMA Animation ephetteplace"
+    echo "Faculty username is optional. Course is created under the Sandboxes category."
     exit
 fi
 
@@ -11,5 +12,10 @@ SANDBOXES_CATEGORY_ID="872"
 DEPARTMENT_CODE="$1"
 COURSE_SHORTNAME="${DEPARTMENT_CODE}-SANDBOX"
 DEPARTMENT_NAME="$2"
+FACULTY_USERNAME="$3"
 
 moosh -n course-create --category=${SANDBOXES_CATEGORY_ID} --fullname="${DEPARTMENT_NAME} Sandbox" --idnumber="${COURSE_SHORTNAME}" "${COURSE_SHORTNAME}"
+
+if [ -n ${FACULTY_USERNAME} ]; then
+    moosh -n course-enrol -r editingteacher -s ${COURSE_SHORTNAME} ${FACULTY_USERNAME}
+fi
