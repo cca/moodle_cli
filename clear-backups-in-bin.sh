4886c1ee46e8f9bd22caf4ac9b43a4fc356953e5
phette23 <phette23@gmail.com>
2020-09-08 13:18:18 -0700

    fix moosh bin reference in clear-backups-in-bin cron job

--- clear-backups-in-bin.sh
+++ clear-backups-in-bin.sh
@@ -3,12 +3,14 @@
 # even though we had automatic backups off. This script is meant to be run from
 # a nightly cron job to clear out the largest of those backup files.
 # report: https://moodle.cca.edu/report/customsql/view.php?id=15
-FILES=$(moosh -n file-list -i 'filename LIKE "%.mbz" AND filesize > 104857600 AND (component = "tool_recyclebin" OR filearea = "recyclebin_course") ORDER BY filesize DESC')
+moosh () { sudo /usr/local/bin/moosh -n $@; }
+
+FILES=$(moosh file-list -i 'filename LIKE "%.mbz" AND filesize > 104857600 AND (component = "tool_recyclebin" OR filearea = "recyclebin_course") ORDER BY filesize DESC')
 
 echo $(date "+%Y-%m-%d %H:%M") 'Deleting the backup files in the recycle bin that are >100mb'
 for FILE in ${FILES}; do
-    moosh -n file-delete ${FILE}
+    moosh file-delete ${FILE}
 done
 
 echo 'Flushing the trash dir'
-moosh -n file-delete --flush
+moosh file-delete --flush
