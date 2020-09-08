23f5d5b80d7ace9ea24c9036f2202c88630ba894
phette23 <phette23@gmail.com>
2020-09-08 12:09:13 -0700

    delete _all_ backups >100mb & print timestamp before starting

--- clear-backups-in-bin.sh
+++ clear-backups-in-bin.sh
@@ -3,10 +3,12 @@
 # even though we had automatic backups off. This script is meant to be run from
 # a nightly cron job to clear out the largest of those backup files.
 # report: https://moodle.cca.edu/report/customsql/view.php?id=15
-FILES=$(moosh -n file-list -i 'filename LIKE "%.mbz" AND filesize > 104857600 AND (component = "tool_recyclebin" OR filearea = "recyclebin_course") ORDER BY filesize DESC' | head -n20)
+FILES=$(moosh -n file-list -i 'filename LIKE "%.mbz" AND filesize > 104857600 AND (component = "tool_recyclebin" OR filearea = "recyclebin_course") ORDER BY filesize DESC')
 
+echo $(date "+%Y-%m-%d %H:%M") 'Deleting the backup files in the recycle bin that are >100mb'
 for FILE in ${FILES}; do
     moosh -n file-delete ${FILE}
 done
 
+echo 'Flushing the trash dir'
 moosh -n file-delete --flush
