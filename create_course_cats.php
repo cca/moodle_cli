fc7da1e6fdbba434fbe88af0f5123ef7da493eb9
Eric Phetteplace <phette23@gmail.com>
2021-07-20 12:45:07 -0700

    make create_course_cats run uninteractively

--- create_course_cats.php
+++ create_course_cats.php
@@ -3,6 +3,8 @@
 // Create our course categories for a semester, which you are prompted to
 // provide by the script. So, for instance, creates the (parent) 2020FA,
 // 2020FA > Metacourses, 2020FA > ANIMA, etc. categories, one per CCA program.
+// Can run interactively with no arguments or accept a single argument that is
+// the parent semester category: `php create_course_cats.php 2021FA`
 
 /**
  *
@@ -22,10 +24,14 @@ require_once($CFG->libdir.'/clilib.php');
 // Spring 2021 changes: DIVST -> ETHNC, VISST -> HAAVC, +GAMES
 $categories = array('Metacourses', 'ANIMA', 'ARCHT', 'ARTED', 'CERAM', 'COMAR', 'COMIC', 'CRAFT', 'CRITI', 'CRTSD', 'CURPR', 'DESGN', 'DIVSM', 'DIVST', 'DSMBA', 'ETHSM', 'ETHST', 'EXCHG', 'EXTED', 'FASHN', 'FILMG', 'FILMS', 'FINAR', 'FNART', 'FURNT', 'FYCST', 'GAMES', 'GELCT', 'GLASS', 'GRAPH', 'HAAVC', 'ILLUS', 'INDIV', 'INDUS', 'INTER', 'IXDGR', 'IXDSN', 'KADZE', 'LITPA', 'MAARD', 'MARCH', 'METAL', 'MOBIL', 'PHCRT', 'PHOTO', 'PNTDR', 'PRINT', 'SCIMA', 'SCULP', 'SFMBA', 'SSHIS', 'TEXTL', 'UDIST', 'VISCR', 'VISST', 'WRITE', 'WRLIT');
 
-$prompt = "Semester category (e.g. 2020FA)";
-$semester_str = trim(cli_input($prompt));
-if (!strlen($semester_str)) {
-    cli_error('Empty semester string, exiting without creating any course categories.');
+if (isset($argv[1])) {
+    $semester_str = trim($argv[1]);
+} else {
+    $prompt = "Semester category (e.g. 2020FA)";
+    $semester_str = trim(cli_input($prompt));
+    if (!strlen($semester_str)) {
+        cli_error('Empty semester string, exiting without creating any course categories.');
+    }
 }
 
 cli_writeln('Creating course categories for ' . $semester_str . ' semester');
