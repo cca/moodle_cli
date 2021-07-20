cf8e4544cc3d7afb6dd331da6cf0a16573aae3f2
Eric Phetteplace <phette23@gmail.com>
2021-07-20 13:20:28 -0700

    rewrite enrollment script to work on GCP
    - no need for a moosh() function anymore as its on the root user's path
    - clean up some sections & comments
    - filter out the 'skipping unknown username' errors
    - don't email admins, the bitnami image doesn't have `mail` installed

--- enrollment_cron.sh
+++ enrollment_cron.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
 
-# pipe fails if any command in it fails (needed for sync_users.php | sed cmd)
+# pipe fails if any command in it fails (needed for sync_users | sed cmd)
 set -o pipefail
-ADMINS="ephetteplace@cca.edu,bobbywhite@cca.edu,nlammiller@cca.edu"
-# we will silence this auth db plugin message (else enroll.log fills up)
-MSG="user with this username was already created through 'cas' plugin."
-moosh () { /opt/moosh/moosh.php -n $@; }
+MOODLE_DIR='/bitnami/moodle'
+# we silence these common but harmless errors (or else the log file fills up)
+CAS_MSG="user with this username was already created through 'cas' plugin."
+UN_MSG="error: skipping unknown user username "
 
 echo "$(date) - running Moodle enrollment script"
-cd /bitnami/moodle
+cd $MOODLE_DIR
 echo 'Setting "unenroll action" to "unenroll user from course"'
-moosh config-set unenrolaction 0 enrol_database
+moosh -n config-set unenrolaction 0 enrol_database
 # we sync users first and then enrollments
 # we enable auth db plugin to sync users, then disable it because we won't want
 # people actually signing in via external database
 # we'll change all their accounts to CAS later
-moosh auth-manage enable db
-php admin/tool/task/cli/schedule_task.php --execute='\auth_db\task\sync_users' | sed "/$MSG/d"
-# unfortunately sync_users.php returns 0 if it cannot access its db but this will
+moosh -n auth-manage enable db
+php admin/tool/task/cli/schedule_task.php --execute='\auth_db\task\sync_users' | sed "/$CAS_MSG/d"
+# @TODO is the below still true with the scheduled task? test on dev
+# unfortunately sync_users returns 0 if it cannot access its db but this will
 # catch some other errors possibly
 SYNC_STATUS=$?
-moosh auth-manage disable db
+moosh -n auth-manage disable db
 php admin/cca_cli/cca_set_cas_logins.php
 
-php admin/tool/task/cli/schedule_task.php --execute='\enrol_database\task\sync_enrolments'
+php admin/tool/task/cli/schedule_task.php --execute='\enrol_database\task\sync_enrolments' | sed "/$UN_MSG/d"
 ENROL_STATUS=$?
 echo 'Setting "unenroll action" back to "keep user enrolled"'
-moosh config-set unenrolaction 1 enrol_database
+moosh -n config-set unenrolaction 1 enrol_database
 
 # add users who are instructors of a course but not already in the FACULTY
 # (id = 2) cohort, nor in the Faculty EXCEPTION (id = 3) cohort, to the Faculty
@@ -41,10 +42,8 @@ echo 'Checking users for instructors who are not in the FACULTY cohort...'
 # (
 #     [id] => 25152
 # )
-# so we use `sed` to get only the ID number itself
-# cannot use moosh() function because SQL phrase becomes un-quoted and treated
-# as 200+ separate arguments
-USERS=$(/opt/moosh/moosh.php -n sql-run 'SELECT user.id FROM {user} user JOIN {role_assignments} ra ON user.id = ra.userid JOIN (SELECT * FROM {context} WHERE contextlevel = 50) context ON ra.contextid = context.id JOIN {role} role ON ra.roleid = role.id LEFT JOIN (SELECT cms.id, cms.userid FROM {cohort_members} cms JOIN {cohort} coh ON cms.cohortid = coh.id WHERE coh.name = "Faculty" OR coh.name = "Faculty Exceptions") cm ON user.id = cm.userid WHERE role.id = 3 AND cm.id IS NULL GROUP BY username ORDER BY username ASC' | sed -n "/\[id\] =>/s/ \+\[id\] => //p")
+# so we use `sed` to get only the user ID numbers
+USERS=$(moosh -n sql-run 'SELECT user.id FROM {user} user JOIN {role_assignments} ra ON user.id = ra.userid JOIN (SELECT * FROM {context} WHERE contextlevel = 50) context ON ra.contextid = context.id JOIN {role} role ON ra.roleid = role.id LEFT JOIN (SELECT cms.id, cms.userid FROM {cohort_members} cms JOIN {cohort} coh ON cms.cohortid = coh.id WHERE coh.name = "Faculty" OR coh.name = "Faculty Exceptions") cm ON user.id = cm.userid WHERE role.id = 3 AND cm.id IS NULL GROUP BY username ORDER BY username ASC' | sed -n "/\[id\] =>/s/ \+\[id\] => //p")
 
 if test -n "${USERS}"; then
     echo "Found $(echo "${USERS}" | wc -l) missing faculty."
@@ -57,11 +56,11 @@ fi
 
 echo "$(date) - enrollment script finished"
 
-# if either command failed, email administrators
+# if either sync task failed, write a notice about it
 if [ $SYNC_STATUS -ne 0 -o $ENROL_STATUS -ne 0 ]; then
-    echo "Script failure. auth/db/cli/sync_users.php status: ${SYNC_STATUS} | enrol/database/cli/sync.php status: ${ENROL_STATUS}"
-    echo "Emailing ${ADMINS}"
-    echo 'See attached log file.' | mail -s 'Moodle: Enrollment Script Error' -a /var/log/moodle/enroll.log $ADMINS
+    echo "A Moodle executed task failed."
+    echo "\auth_db\task\sync_users exit code: ${SYNC_STATUS}"
+    echo "\enrol_database\task\sync_enrolments exit code: ${ENROL_STATUS}"
 fi
 
 echo
