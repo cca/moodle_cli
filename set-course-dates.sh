61ff5900b4e3e9a85a9942520b88dfd493751049
Eric Phetteplace <phette23@gmail.com>
2021-10-28 12:19:45 -0700

    add script to bulk change course start/end dates

---
+++ set-course-dates.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# set the start & end date for all courses within a category
+# https://moosh-online.com/commands/
+START=$1
+END=$2
+# default to Course Templates > Program Templates category
+CATEGORY=${CATEGORY:-877}
+
+moosh () { /usr/bin/moosh -n $@; }
+
+if [ -z ${START} -o -z ${END} ]; then
+    echo -e 'Error: you must provide both a start and end date in UNIX timestamps. Category defaults to 877 (Program Templates). Usage example:\n\n\tCATEGORY=877 ./set-course-date.sh 1610956800 1621061940' >&2
+    exit 1
+fi
+
+moosh course-config-set category ${CATEGORY} startdate ${START}
+moosh course-config-set category ${CATEGORY} enddate ${END}
