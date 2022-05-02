1031f2649fc3d4d6e2f1e9a7d76131d9d02f80b4
phette23 <phette23@gmail.com>
2022-05-02 13:27:21 -0700

    draft of script to enrol user in all of a program's courses

---
+++ enroll_in_all.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# enroll a user in all of a program's courses in a particular semester
+# including metacourses
+usage () {
+    echo -e 'Usage:\n\t./enroll_in_all.sh $SEMESTER $PROGRAM $USER [ $ROLE ]\n'
+    echo -e 'Examples:\n\t./enroll_in_all.sh 2022SP GAMES ephetteplace'
+    echo -e '\t./enroll_in_all.sh 2022FA ANIMA nchan exportonlyteacher\n'
+    echo 'ROLE defaults to editingteacher if not provided.'
+    echo 'All other arguments are required.'
+}
+
+if [[ -z ${1} || ${1} = "-h" || ${1} = "--help" || ${1} = "help" ]]; then
+    usage
+    exit 0
+fi
+
+SEMESTER=$1
+PROGRAM=$2
+USER=$3
+ROLE=${4:-editingteacher}
+
+if [[ -z ${SEMESTER} || -z ${PROGRAM} || -z ${USER} ]]; then
+    echo -e "ERROR: semester, program, and user arguments are required.\n" 1>&2
+    usage
+    exit 1
+fi
+
+cd /bitnami/moodle
+# Ensure moosh is on PATH
+export PATH=${PATH}:/usr/bin
+
+echo "Enrolling user ${USER} in all ${SEMESTER} ${PROGRAM} courses with role ${ROLE}"
+
+for course in $(moosh -n course-list -i "shortname LIKE \"%${PROGRAM}%\"-${SEMESTER}"); do
+    moosh -n course-enrol -r ${ROLE} ${course} ${USER}
+done
