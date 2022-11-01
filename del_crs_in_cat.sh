14f045b0721ff2c126e44084404f819b8f6693ad
phette23 <phette23@gmail.com>
2022-11-01 16:31:53 -0700

    shellcheck fixes on del_crs_in_cat ref #4

--- del_crs_in_cat.sh
+++ del_crs_in_cat.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 # delete all courses in a category, usage:
 # ./del-crs-in-cat.sh $CATEGORY_NAME
-moosh () { /usr/bin/moosh -n $@; }
 
 if [[ -n $1 ]]; then
-    CATEGORY=$(moosh category-list $1 | tail -n1 | cut -d ' ' -f 1)
+    CATEGORY=$(moosh category-list "$1" | tail -n1 | cut -d ' ' -f 1)
     if [[ -n $CATEGORY ]]; then
-        moosh course-list -c $CATEGORY -i | xargs moosh -n course-delete
+         /usr/bin/moosh -n course-list -c "$CATEGORY" -i | xargs /usr/bin/moosh -n course-delete
     else
         echo "Unable to find category with name = $1"
         exit 1
