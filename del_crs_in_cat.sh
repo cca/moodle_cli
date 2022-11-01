06b7f9feedfc4bf7ddeb461270b7d40fa38763d0
phette23 <phette23@gmail.com>
2022-11-01 16:33:11 -0700

    fix moosh -n user warning without the shell function

--- del_crs_in_cat.sh
+++ del_crs_in_cat.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 # delete all courses in a category, usage:
 # ./del-crs-in-cat.sh $CATEGORY_NAME
+moosh () { /usr/bin/moosh -n $@; }
 
 if [[ -n $1 ]]; then
     CATEGORY=$(moosh category-list "$1" | tail -n1 | cut -d ' ' -f 1)
     if [[ -n $CATEGORY ]]; then
-         /usr/bin/moosh -n course-list -c "$CATEGORY" -i | xargs /usr/bin/moosh -n course-delete
+        # shell functions can't be passed to xargs so we have to use the path to moosh
+        moosh course-list -c "$CATEGORY" -i | xargs /usr/bin/moosh -n course-delete
     else
         echo "Unable to find category with name = $1"
         exit 1
