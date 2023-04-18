6efae454a16b357d5ff11e32911c9d3efb4f22c6
phette23 <phette23@gmail.com>
2023-04-18 15:12:01 -0700

    update moosh paths in foundations.sh for v4

--- foundations.sh
+++ foundations.sh
@@ -27,7 +27,7 @@ create_course () {
 
     # create a course & store the created ID number, which we have to `grep`
     # for because moosh includes error text in stdout
-    ID=$(/usr/bin/moosh -n course-create --category="${SANDBOXES_CATEGORY_ID}" \
+    ID=$(moosh -n course-create --category="${SANDBOXES_CATEGORY_ID}" \
         --fullname="${SURNAME} Practice Course" \
         --idnumber="${COURSE_ID}" "${COURSE_ID}" 2>/dev/null | grep -x '^[0-9]*$')
 
@@ -37,11 +37,11 @@ create_course () {
         # enrol user as instructor and a test student
         # set the course start date to the past, no end date, & make it visible
         # 1628406000 => 2021-08-08 00:00 PT
-        /usr/bin/moosh -n course-enrol -r editingteacher -s "${COURSE_ID}" "${USERNAME}" \
-        && /usr/bin/moosh -n course-enrol -s "${COURSE_ID}" stest \
-        && /usr/bin/moosh -n course-config-set course "${ID}" startdate 1628406000 \
-        && /usr/bin/moosh -n course-config-set course "${ID}" enddate 0 \
-        && /usr/bin/moosh -n course-config-set course "${ID}" visible 1 \
+        moosh -n course-enrol -r editingteacher -s "${COURSE_ID}" "${USERNAME}" \
+        && moosh -n course-enrol -s "${COURSE_ID}" stest \
+        && moosh -n course-config-set course "${ID}" startdate 1628406000 \
+        && moosh -n course-config-set course "${ID}" enddate 0 \
+        && moosh -n course-config-set course "${ID}" visible 1 \
         && echo "Successfully created & configured practice course for ${USERNAME}"
     fi
 }
--- foundations_cron.sh
+++ foundations_cron.sh
@@ -2,12 +2,14 @@
 # Create practice courses, using the foundations.sh script, for all students
 # enrolled in the Moodle Foundations (id=2116) course.
 # Run on a nightly schedule
-export PATH="/opt/bitnami/php/bin:$PATH"
+# @TODO check PATH on production, some of these may not be needed
+export PATH="/opt/bitnami/php/bin:/usr/bin:/usr/local/bin:$PATH"
 export TZ="America/Los_Angeles"
 cd /bitnami/moodle || (echo "Error: unable to cd into /bitnami/moodle, does directory exist?" >&2 || exit)
 # List of student usernames, ignoring test accounts
 # NOTE: `sed` must use extended regular expressions (-E)
-STUDENTS=$(/usr/bin/moosh -n user-list --course 2116 --course-role student \
+FOUNDATIONS_ID=$(moosh -n course-list -i "fullname LIKE '%Moodle Foundations%'")
+STUDENTS=$(moosh -n user-list --course ${FOUNDATIONS_ID} --course-role student \
     | cut -f 1 -d ' ' | sed -E '/^(s|f)test1?$/d')
 
 date
