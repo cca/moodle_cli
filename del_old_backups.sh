3c4fdb05d251ab259c772f90729a07aa16f7b350
phette23 <phette23@gmail.com>
2024-01-18 17:09:21 -0800

    improve del_old_backups

--- del_old_backups.sh
+++ del_old_backups.sh
@@ -2,9 +2,13 @@
 # Identify backups older than 1 year old and delete them
 cd /bitnami/moodle || (echo "Error: unable to cd into /bitnami/moodle, does directory exist?" >&2 || exit)
 # Ensure moosh is on PATH
-export PATH=${PATH}:/usr/bin:/usr/local/bin
+export PATH=${PATH}:/bin:/usr/bin:/usr/local/bin
+QUERY="component='backup' and timecreated<$(date -d '1 year ago' +%s)"
 echo "$(date)" "deleting backup files more than a year old"
-echo "DRY RUN files will not be deleted"
-moosh -n file-list -i "component='backup' and timecreated<$(date -d '1 year ago' +%s)" \
-    xargs -n1 echo
-    # xargs -n1 moosh -n file-delete
+if [[ "$1" != "-d" && "$1" != "--delete" ]]; then
+    echo "DRY RUN files will not be deleted. Add a -d or --delete flag to delete files"
+    moosh -n file-list "${QUERY}"
+else
+    echo "Deleting files"
+    moosh -n file-list -i "${QUERY}" | xargs -n1 nice moosh -n file-delete
+fi
