430303482bb52ed64816ec28a2de2f42caaff6b0
phette23 <phette23@gmail.com>
2024-04-19 11:39:34 -0700

    feat: set_course_dates accepts date strings instead of UNIX seconds

--- set_course_dates.sh
+++ set_course_dates.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 # set the start & end date for all courses within a category
-# https://moosh-online.com/commands/
+# usage: ./set_course_dates.sh "2024-01-01" "2024-05-15"
+
+# Bash strict mode
+set -euo pipefail
+
 START=$1
 END=$2
 # default to Course Templates > Program Templates category
 CATEGORY=${CATEGORY:-877}
 
-if [ -z "${START}" ] || [ -z "${END}" ]; then
-    echo -e 'Error: you must provide both a start and end date in UNIX timestamps. Category defaults to 877 (Program Templates). Usage example:\n\n\tCATEGORY=877 ./set-course-date.sh 1610956800 1621061940' >&2
-    exit 1
-fi
+date_to_unix() {
+    # convert timestamp to PST
+    date -d "$1" +%s | awk '{print $1 - 8*60*60}'
+}
+
+# convert dates to UNIX timestamps
+START=$(date_to_unix "${START}")
+END=$(date_to_unix "${END}")
 
 moosh -n course-config-set category "${CATEGORY}" startdate "${START}"
 moosh -n course-config-set category "${CATEGORY}" enddate "${END}"
