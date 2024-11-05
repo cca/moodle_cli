f5f3d76a25aba344c4b0230107205664c1a717ba
phette23 <phette23@gmail.com>
2024-11-04 16:56:11 -0800

    docs: comment improvements for bulk_role_change

--- bulk_role_change.sh
+++ bulk_role_change.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 # sets enrollments of a series of courses (e.g. a whole category) to a given role
-# can use the MOODLE_ROLE environment variable. Used in Fall 2021 to bulk change
-# all the 2021SP template course enrollments to a read-only role. It is probably
-# easiest to just edit this script to do the role/courses list (e.g. using a
-# `moosh -n course-list` query) rather than make it take CLI arguments.
+# can use the MOODLE_ROLE environment variable. We use this every semester to set
+# all template course enrollments to EOI or to Instructor.
 ROLE=${MOODLE_ROLE:-exportonlyteacher}
 # default to Course Templates > Program Templates category
 CATEGORY=${MOODLE_CATEGORY:-877}
 
 COURSES=$(moosh -n course-list -c "${CATEGORY}" -i)
 
+# shellcheck disable=SC2068
 for COURSE in ${COURSES[@]}; do
     # get all enrolled users, unenroll them, re-enroll them in the chosen role
     echo "Setting all users enrolled in course ID ${COURSE} to '${ROLE}'"
-    IDS=$(moosh -n user-list -i --course="${COURSE}")
+    IDS=$(moosh -n user-list --id --course="${COURSE}")
     if [[ -n $IDS ]]; then
+        # shellcheck disable=SC2086
         moosh -n course-unenrol "${COURSE}" ${IDS}
-        moosh -n course-enrol -r "${ROLE}" -i "${COURSE}" ${IDS}
+        # shellcheck disable=SC2086
+        moosh -n course-enrol -r "${ROLE}" --id "${COURSE}" ${IDS}
     fi
 done
