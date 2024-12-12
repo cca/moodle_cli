a706243e1af5cc38249ae1d1e40b44404290b3cf
phette23 <phette23@gmail.com>
2024-12-12 13:56:14 -0800

    fix: improvements for foundations practice course scripts
    fix test student username
    refactor a little
    no more help for foundations.sh nor CSV usage mode that's not relevant
    check if course already exists instead of trying to create it no matter what
    check if Foundations course was found before continuing
    look up Foundations category once instead of on each course creation

--- foundations.sh
+++ foundations.sh
@@ -1,46 +1,30 @@
 #!/usr/bin/env bash
-
-if [[ "$1" = "-h" || "$1" =~ "-help" || -z "$1" ]]; then
-    echo -e "Create sandbox course for faculty enrolled in Moodle Foundations.
-
-Usage:
-    ./foundations.sh faculty.csv
-    ./foundations.sh USERNAME [SURNAME]
-
-USERNAME is the faculty member's CCA username (not email address, no
-'@cca.edu'). SURNAME can have spaces in it but would need to quoted in that
-case, e.g., 'Van Halen'. If you don't provide a surname then it defaults to
-the USERNAME. The faculty.csv file is a list of these faculty username &
-surname pairs separated by commas, e.g., 'ephetteplace,Phetteplace'."
-    exit 0
-fi
-
-# moosh category-list claims it can accept a "search" parameter but
-# in my testing that did not work so this is the best we can do
-SANDBOXES_CATEGORY_ID=$(moosh -n category-list | grep FOUNDATIONS | cut -f 1 -d ' ')
+# Create a practice course in CATEGORY for USERNAME
 
 create_course () {
-    USERNAME="$1"
-    SURNAME="$2"
-    if [ -z "${SURNAME}" ]; then
-        SURNAME="${USERNAME}"
-    fi
+    CATEGORY="$1"
+    USERNAME="$2"
     COURSE_ID="SANDBOX-${USERNAME}"
+
+    EXISTING_COURSE=$(moosh -n course-list "shortname = '$COURSE_ID'" | tr -d '\n')
+    if [[ -n "$EXISTING_COURSE" ]]; then
+        return 0
+    fi
 
     # create a course & store the created ID number, which we have to `grep`
     # for because moosh includes error text in stdout
-    ID=$(moosh -n course-create --category="${SANDBOXES_CATEGORY_ID}" \
-        --fullname="${SURNAME} Practice Course" \
+    ID=$(moosh -n course-create --category="${CATEGORY}" \
+        --fullname="${USERNAME} Practice Course" \
         --idnumber="${COURSE_ID}" "${COURSE_ID}" 2>/dev/null | grep -x '^[0-9]*$')
 
     # if we created a new course, configure it
     if [[ $? && -n "${ID}" ]]; then
-        echo "Created course ${ID} '${SURNAME} Practice Course'"
+        echo "Created course ${ID} ${USERNAME} Practice Course"
         # enrol user as instructor and a test student
         # set the course start date to the past, no end date, & make it visible
         # 1628406000 => 2021-08-08 00:00 PT
         moosh -n course-enrol -r editingteacher -s "${COURSE_ID}" "${USERNAME}" \
-        && moosh -n course-enrol -s "${COURSE_ID}" stest \
+        && moosh -n course-enrol -s "${COURSE_ID}" library-test-student-1 \
         && moosh -n course-config-set course "${ID}" startdate 1628406000 \
         && moosh -n course-config-set course "${ID}" enddate 0 \
         && moosh -n course-config-set course "${ID}" visible 1 \
@@ -48,16 +32,4 @@ create_course () {
     fi
 }
 
-# case 1: first argument is a CSV of faculty
-if [[ -f "$1" ]]; then
-    grep -v '^ *#' "$1" | while IFS= read -r LINE; do
-        if [[ -n ${LINE} ]]; then
-            USERNAME=$(echo "${LINE}" | cut -d , -f 1)
-            SURNAME=$(echo "${LINE}" | cut -d , -f 2)
-            create_course "${USERNAME}" "${SURNAME}"
-        fi
-    done
-else
-    # case 2: only a single course
-    create_course "$1" "$2"
-fi
+create_course "$1" "$2"
--- foundations_cron.sh
+++ foundations_cron.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 # Create practice courses, using the foundations.sh script, for all students
 # enrolled in the Moodle Foundations (id=2116) course.
-# Run on a nightly schedule
+# Runs on a nightly schedule
 export PATH="/opt/bitnami/php/bin:/usr/bin:/usr/local/bin:$PATH"
 export TZ="America/Los_Angeles"
 cd /bitnami/moodle || (echo "Error: unable to cd into /bitnami/moodle, does directory exist?" >&2 || exit)
 # List of student usernames, ignoring test accounts
 # ! `sed` must use extended regular expressions (-E)
 FOUNDATIONS_ID=$(moosh -n course-list -i "shortname = 'Moodle-Foundations-101'")
+if [[ -z "$FOUNDATIONS_ID" ]]; then
+    echo "Error: unable to find Moodle Foundations course." >&2
+    exit 1
+fi
 # omit test students even though these are usually in an EOI & not student role
 STUDENTS=$(moosh -n user-list --course "${FOUNDATIONS_ID}" --course-role student \
     | cut -f 1 -d ' ' | sed -E '/^library-test-/d')
+SANDBOXES_CATEGORY_ID=$(moosh -n category-list FOUNDATIONS | grep FOUNDATIONS | cut -f 1 -d ' ')
 
 date
 echo "Creating practice courses for Moodle Foundations. There are" "$(echo "${STUDENTS}" | wc -w)" "students."
 
 for STUDENT in ${STUDENTS}; do
-    /bitnami/moodle/admin/cca_cli/foundations.sh "${STUDENT}"
+    /bitnami/moodle/admin/cca_cli/foundations.sh "${SANDBOXES_CATEGORY_ID}" "${STUDENT}"
 done
 
 # see note at bottom of enrollment_cron.sh for explanation
